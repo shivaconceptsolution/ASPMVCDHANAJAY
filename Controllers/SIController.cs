5445ba6e1e194f724cc7f973763b9c0fcd2cdc73
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-08-24 12:11:22 +0530

    Add files via upload

---
+++ Controllers/SIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication31.Controllers
+{
+    public class SIController : Controller
+    {
+        // GET: SI
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection obj)
+        {
+            float p = float.Parse(obj["txtp"]);
+            float r = float.Parse(obj["txtr"]); ;
+            float t = float.Parse(obj["txtt"]); ;
+            ViewBag.si = (p * r * t) / 100;
+            return View();
+        }
+    }
+}
